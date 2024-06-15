56606db043290a355da8d8875918f6d23edd8f81
Stanley Boateng <stankofb@gmail.com>
2024-06-15 14:01:50 +0000

    "Added search bar with input field and filter icon, and a featured jobs section to Home screen"

--- homeScreen.js
+++ homeScreen.js
@@ -1,5 +1,8 @@
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput } from "react-native";
+
+const featuredJobs =
 
 export default function Home({ route }) {
   const { name, email } = route.params || {};
@@ -18,7 +21,21 @@ export default function Home({ route }) {
               />
             </TouchableOpacity>
         </View>
-        
+        <View style={styles.searchBar}>
+            <TextInput style={styles.input} placeholder="Search a job or position" />
+            <View style={styles.filterIcon}>
+              <TouchableOpacity>
+                <Image
+                  source={require("./assets/filter.png")}
+                  style={{width: 26, height:26}}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={{flexDirection: "row", justifyContent: 'space-between', alignItems: 'center', top: 100, margin: 24}}>
+            <Text style={{fontSize: 16, fontWeight:'bold', color: '#ODOD26', lineHeight: 20.8}}>Featured Jobs</Text>
+            <Text style={{fontsiez: 14, fontWeight: 400, color: '#95969D'}}>See all</Text>
+          </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -33,7 +50,7 @@ const styles = StyleSheet.create({
    flexDirection: 'row',
    alignItems: 'center',
    justifyContent: 'space-between',
-   top: 64,
+   top: 50,
    width: 254,
    height: 55,
    left: 24,
@@ -54,6 +71,32 @@ const styles = StyleSheet.create({
   profilePic:{
     width: 56,
     height: 56,
+    left: 120,
 
   },
+  searchBar:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 368,
+    margin: 24,
+    top: 100,
+    
+  },
+  input:{
+    backgroundColor: "#F2F2F3",
+    width: "80%",
+    height: 48,
+    borderRadius: 12,
+    padding: 8,
+  },
+  filterIcon:{
+    backgroundColor: "#F2F2F3",
+    width: 50,
+    height: 48,
+    padding: 10,
+    borderRadius: 14,
+    alignItems: 'center'
+  }
 });
